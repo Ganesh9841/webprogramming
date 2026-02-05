3f3dfc86adb57ad92ff89471eb98437d5fae8737
Ganesh Wagle <wagleganesh5@gmail.com>
2026-02-05 10:07:29 +0545

    updated appjs

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('hello world')
+document.querySelector('h1').innerText = 'this text is changed'
+console.log(document);
+console.log(document.title);
+console.log(document.URL);
+alert('hello from app.js');
+
+
